5b3b023b898ef4ba296b585cfe464959ae4c34ff
kajaakajaa <rwqxh8703@yahoo.co.jp>
2022-12-06 12:16:29 +0900

    2022/12/6(火)/16mac/modal骨組み完了

--- component/Header.js
+++ component/Header.js
@@ -1,19 +1,52 @@
 import { Link as Scroll } from 'react-scroll';
+import Link from 'next/link';
 import Image from 'next/image';
 import headerStyles from '../styles/header.module.css';
 import React, { useRef } from 'react';
 
 export default function Header(props) {
-  const hamburgerFlag = useRef(null);
+  const modalFlag = useRef(null);
+  const modalBody = useRef(null);
+  const modalFooter = useRef(null);
+  const Modal = useRef(null);
+  const Overlay = useRef(null);
 
-  function overLayClose() {}
-  function checkSignOut() {}
+  function overLayClose() {
+    Modal.current.animate({
+      transform: ['translateY(0px)', 'translateY(-50px)'],
+      visibility: ['visible', 'hidden'],
+      opacity: [1, 0]
+    },{
+      fill: 'forwards',
+      duration: 150
+    });
+    modalFlag.current.checked = false;
+  }
+
+  function checkSignOut() {
+    modalFlag.current.checked = true;
+    Overlay.current.style.zIndex = 2;
+    Modal.current.animate({
+      transform: ['translateY(-50px)', 'translateY(0px)'],
+      visibility: ['hidden', 'visible'],
+      opacity: [0, 1]
+    },{
+      fill: 'forwards',
+      duration: 150
+    });
+  }
+
   function checkAcountDelete() {}
 
   return(
     <>
-      <input type="checkbox" id="hamburger_flag" className={headerStyles.hamburgerFlag} ref={hamburgerFlag} />
-      <div className={headerStyles.overlay} onClick={overLayClose}></div>
+      <input type="checkbox" id="hamburger_flag" className={headerStyles.modalFlag} ref={modalFlag} />
+      <div className={headerStyles.overlay} ref={Overlay} onClick={overLayClose}>
+        <div className={headerStyles.modal} onClick={(e)=> {e.preventDefault(); e.stopPropagation();}} ref={Modal}>
+          <div className={headerStyles.modalBody} ref={modalBody}></div>
+          <div className={headerStyles.modalFooter} ref={modalFooter}></div>
+        </div>
+      </div>
       <header>
         <div className={headerStyles.headerLogoWrapper}>
           <Scroll to="to" smooth={true} duration={600}>
@@ -21,8 +54,8 @@ export default function Header(props) {
           </Scroll>
         </div>
         <div className={headerStyles.signAreaWrapper} ref={props.signArea}>
-          <button onClick={checkSignOut} className={headerStyles.signOutBtn}>ログアウト</button>
-          <button onClick={checkAcountDelete} className={headerStyles.acountDeleteBtn}>退会</button>
+          <p onClick={checkSignOut} className={headerStyles.signOutBtn}>ログアウト</p>
+          <p onClick={checkAcountDelete} className={headerStyles.acountDeleteBtn}>退会</p>
         </div>
       </header>
     </>
