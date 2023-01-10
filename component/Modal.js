7bd31828427d824f7ca778cd874c86e230e20280
kajaakajaa <rwqxh8703@yahoo.co.jp>
2023-01-10 19:14:25 +0900

    2023/1/10(火)/16mac/modalコンポーネントの関数 → index(最上位)に移植。削除ボタン実装完了

--- component/Modal.js
+++ component/Modal.js
@@ -1,90 +1,22 @@
 import modalStyles from '../styles/modal.module.css';
-import React, { useRef, useContext, useEffect } from 'react';
+import React, { useRef, useContext, useEffect, useState } from 'react';
 import { IndexContainerContext } from '../pages/';
 import { RegistItemResponsive } from '../component/index';
 
 export default function Modal() {
   const indexValue = useContext(IndexContainerContext);
 
-  function overLayClose() {
-    if(indexValue.Overlay.current.id == 'check_sign_out_overlay' || indexValue.Overlay.current.id == 'delete_acount_overlay' ||
-        indexValue.Overlay.current.id == 'check_deleteBtn_overlay') {
-      indexValue.modalRef.current.animate({
-        transform: ['translateY(0px)', 'translateY(-50px)'],
-        visibility: ['visible', 'hidden'],
-        opacity: [1, 0]
-      },{
-        fill: 'forwards',
-        duration: 150
-      });
-      indexValue.modalFlag.current.checked = false;
-      indexValue.Overlay.current.style.zIndex = 'auto';
-      indexValue.Overlay.current.id = '';
-      indexValue.modalBody.current.innerHTML = '';
-      indexValue.hamburgerOpenflag.current.checked = false;
-      indexValue.yesBtn.current.id = '';
-    }
-    else if(indexValue.Overlay.current.id == 'hamburger_open_overlay') {
-      indexValue.modalFlag.current.checked = false;
-      indexValue.Overlay.current.style.zIndex = 'auto';
-      indexValue.Overlay.current.id = '';
-      indexValue.hamburgerOpenflag.current.checked = false;
-    }
-    else if(indexValue.Overlay.current.id == 'check_registBtn_responsive_overlay') {
-      indexValue.modalFlag.current.checked = false; //overlay非表示
-      indexValue.Overlay.current.id = '';
-      //レスポンシブの新規商品登録フォームの非表示
-      indexValue.RegistItemResponsiveWrapper.current.animate({
-        opacity: [1, 0],
-        transform: ['translateY(0)', 'translateY(-50px)']
-      }, {
-        fill: 'forwards',
-        duration: 150
-      });
-      
-      setTimeout(()=> {
-        indexValue.RegistItemResponsiveWrapper.current.style.display = 'none';
-        Object.assign(indexValue.Overlay.current.style, {
-          zIndex: 'auto',
-          display: 'flex'
-        });
-      }, 100);
-      indexValue.resetAll();
-    }
-  }
-
-  function modalBtn(e, status) {
-    if(status == 'yes') {
-      let id = e.target.id;
-      if(id == 'check_sign_out') {
-        overLayClose();
-      }
-      else if(id == 'delete_acount') {
-        overLayClose();
-      }
-      else if(id == 'check_deleteBtn_yes') {
-        // (async()=> {
-        //   let query = {};
-        //       query['user_id'] = user
-        // })();
-      }
-    }
-    else if(status == 'no') {
-      overLayClose();
-    }
-  }
-
   return(
     <>{/* ↓ ref=modalFLag → overlay表示フラグ*/}
       <input type="checkbox" id="hamburger_flag" className={modalStyles.modalFlag} ref={indexValue.modalFlag} />
-      <div className={modalStyles.overlay} ref={indexValue.Overlay} onClick={overLayClose}>
+      <div className={modalStyles.overlay} ref={indexValue.Overlay} onClick={indexValue.overLayClose}>
         <RegistItemResponsive />
         <div className={modalStyles.modal} onClick={(e)=> {e.preventDefault(); e.stopPropagation();}} ref={indexValue.modalRef}>
           <div className={modalStyles.modalBody} ref={indexValue.modalBody}></div>
           <div className={modalStyles.modalFooter} ref={indexValue.modalFooter}>
             <div>
-              <button onClick={(e)=> {modalBtn(e, 'yes')}} ref={indexValue.yesBtn}>yes</button>
-              <button onClick={(e)=> {modalBtn(e, 'no')}}>no</button>
+              <button onClick={(e)=> {indexValue.modalBtn(e, 'yes')}} ref={indexValue.yesBtn}>yes</button>
+              <button onClick={(e)=> {indexValue.modalBtn(e, 'no')}}>no</button>
             </div>
           </div>
         </div>
--- pages/api/request/index.js
+++ pages/api/request/index.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   const { mode } = req.query;
+  let response = '';
 
   switch(mode) {
     case 'set_list_data':
@@ -12,13 +13,23 @@ export default async function handler(req, res) {
     break;
 
     case 'regist_item':
-      const response_regist = await fetch('https://kajaaserver.com/kaidashi_php/sql_data.php?mode=regist_item', {
+        response = await fetch('https://kajaaserver.com/kaidashi_php/sql_data.php?mode=regist_item', {
         method: 'POST',
         body: JSON.stringify(req.body),
         headers: {'Content-type': 'application/json'}
       });
-      const regist_data_from_php = await response_regist.json();
+      const regist_data_from_php = await response.json();
       res.status(200).json({regist_data_from_php});
+    break;
+
+    case 'delete_item':
+      fetch('https://kajaaserver.com/kaidashi_php/sql_data.php?mode=delete_item', {
+        method: 'POST',
+        body: JSON.stringify(req.body),
+        headers: {'Content-type': 'application/json'}
+      })
+      // const delete_result = await response.json();
+      // res.status(200).json({delete_result});
     break;
   }
 }
